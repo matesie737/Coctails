60777fc41766b90b9f7f17931f646e60d28ed58c
matesie737 <mateusz.siedlaczek@gmail.com>
2022-01-16 20:27:07 +0100

    home page lorum impsum

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,11 +1,41 @@
-import style from './style/HomePage.module.css'
+import style from "./style/HomePage.module.css";
+import informations from "./../../exampleData/informations.json";
 
 function HomePage() {
+  const Informations = informations.map((data, id) => {
 
-    return (
-        <>
-            <p className={style.main}>HomePage</p>
-        </>
+    return ( id%2 ? (
+      <div className={style.container} style={{ animationDelay: `${(id + 1) * 0.5}s` }}>
+        <div className={style.item}>
+          <div className={style.photo} style={{ animationDelay: `${(id + 5) * 0.5}s` }}>t</div>
+          <div className={style.textbox}>
+            <div className={style.title}>
+              <p>{data.title}</p>
+            </div>
+            <div className={style.text}>{data.text}</div>
+          </div>
+        </div>
+      </div>) : 
+      (
+        <div className={style.container} style={{ animationDelay: `${ (id + 1) * 0.5}s` }}>
+          <div className={style.item}>
+            <div className={style.textbox}>
+              <div className={style.title}>
+                <p>{data.title}</p>
+              </div>
+              <div className={style.text}>{data.text}</div>
+            </div>
+            <div className={style.photo } style={{ animationDelay: `${(id + 1) * 0.5}s` }}>t</div>
+          </div>
+        </div>)
     );
+  });
+
+  return (
+    <>
+      <p className={style.greeting}>World of Coctails!</p>
+      {Informations}
+    </>
+  );
 }
 export default HomePage;
